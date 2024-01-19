2f093a6e54947703433094dae6856432c50e5ccc
psavarmattas <puranjaysavarmattas@gmail.com>
2024-01-19 23:53:11 +0000

    Added: A fucntion to check dependencies, TensorFlow setup, and initiate the model training or loading based on user input.

---
+++ main.py
@@ -0,0 +1,30 @@
+from utils.check_dependencies import *
+from utils.check_tf import *
+from utils.train_load_finish import *
+
+def main():
+
+    """
+    Check dependencies, TensorFlow setup, and initiate the model training or loading based on user input.
+
+    This script checks for all necessary dependencies, verifies the TensorFlow installation,
+    and prompts the user to choose between training the model or loading an existing one.
+
+    Returns:
+        None
+    """
+
+    # Check all dependencies
+    check_dependencies()
+
+    # Checking TensorFlow and its dependencies
+    check_tf()
+
+    # Asking the user if they want to train the model or not.
+    print("====================================================================================================")
+    train_model = str(input("Do you want to train the model? (Y/N): "))
+    print("====================================================================================================")
+    train_or_load(train_model)
+
+if __name__ == "__main__":
+    main()
