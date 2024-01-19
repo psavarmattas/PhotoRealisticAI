edbf7491f076c56172a9ff88d4c4fa771da1b1ca
psavarmattas <puranjaysavarmattas@gmail.com>
2024-01-19 22:46:10 +0000

    Added: A function to check if TensorFlow is installed and detect available CPUs and GPUs

---
+++ utils/check_tf.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+def check_tf():
+
+    """
+    Check if TensorFlow is installed and detect available CPUs and GPUs.
+
+    This function prints information about the TensorFlow version, the list of available CPUs,
+    the number of available CPUs, the list of available GPUs, and the number of available GPUs.
+
+    Returns:
+    None
+    """
+
+    print("====================================================================================================")
+    print("Check if Tensorflow is installed & CPUs & GPUs are available")
+    print("====================================================================================================")
+
+    print("Tensorflow Version Available: ", tf.version.VERSION)
+    print("\n", tf.config.list_physical_devices('CPU'))
+    print("\nNum CPUs Available: ", len(tf.config.list_physical_devices('CPU')))
+    print("\n", tf.config.list_physical_devices('GPU'))
+    print("\nNum GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
