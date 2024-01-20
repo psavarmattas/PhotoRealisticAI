bf844e9f0200e92b515c2f532b9cdc0a633761b8
psavarmattas <puranjaysavarmattas@gmail.com>
2024-01-20 12:57:12 +0000

    Update: Better user warnings for GPU, CPU & TF versions

--- utils/check_tf.py
+++ utils/check_tf.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import sys
 
 def check_tf():
 
@@ -17,7 +18,17 @@ def check_tf():
     print("====================================================================================================")
 
     print("Tensorflow Version Available: ", tf.version.VERSION)
-    print("\n", tf.config.list_physical_devices('CPU'))
-    print("\nNum CPUs Available: ", len(tf.config.list_physical_devices('CPU')))
-    print("\n", tf.config.list_physical_devices('GPU'))
-    print("\nNum GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    if tf.version.VERSION >= '2.0':
+        if len(tf.config.list_physical_devices('GPU')) >= 2:
+            print("\n", tf.config.list_physical_devices('GPU'))
+            print("\nNum GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+        elif len(tf.config.list_physical_devices('CPU')) >= 2:
+            print("\n", tf.config.list_physical_devices('CPU'))
+            print("\nNum CPUs Available: ", len(tf.config.list_physical_devices('CPU')))
+            print("\nNo GPUs found therefore training will be done using CPU.")
+        else:
+            print("\n>>> No GPU/CPU found. Please check if TensorFlow and it's addons are installed correctly.")
+            sys.exit(1)
+    else:
+        print("The current Tensorflow version is less than v2.0. Please upgrade to v2.0 or higher to continue.")
+        sys.exit(1)
