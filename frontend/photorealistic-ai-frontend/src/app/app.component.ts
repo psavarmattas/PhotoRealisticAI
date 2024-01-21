5f2c63a565bec03b532c3c0b5bfda6dd454ca159
psavarmattas <puranjaysavarmattas@gmail.com>
2024-01-21 15:09:30 +0000

    Update: Add styles and dependencies for generating and displaying images. Introduced Primeng.

--- frontend/photorealistic-ai-frontend/src/app/app.component.ts
+++ frontend/photorealistic-ai-frontend/src/app/app.component.ts
@@ -2,29 +2,31 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { error } from 'console';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HttpClientModule],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.scss',
   providers: [],
 })
 export class AppComponent {
-  constructor(public httpservice: HttpClient,public cdr: ChangeDetectorRef) {}
+  constructor(public httpservice: HttpClient, public cdr: ChangeDetectorRef) {}
   title = 'PhotoRealistic AI';
   imageSrc: any = null;
+  messages: any = null;
 
   // Handle button click to load a new image from the API
   loadImage() {
-    this.httpservice.get("http://127.0.0.1:5000/generate_image").subscribe((res:any)=>{
-      this.imageSrc = 'data:image/png;base64,' + res['image'];
-      // this.imageSrc = atob(res['image']);÷
-      this.cdr.detectChanges();
-      this.cdr.markForCheck();
-    },
-    (error:any)=>console.log(error));
+    this.httpservice.get('http://127.0.0.1:5000/generate_image').subscribe(
+      (res: any) => {
+        this.imageSrc = 'data:image/png;base64,' + res['image'];
+        this.cdr.detectChanges();
+        this.cdr.markForCheck();
+      },
+      (error: any) => console.log(error)
+    );
   }
 }
