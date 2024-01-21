1c8ada3a76543e68406dc9c3fe18b0f9df95fa81
psavarmattas <puranjaysavarmattas@gmail.com>
2024-01-21 14:28:16 +0000

    Update:  To show generated image added HttpClientModule and HttpClient to app.component.ts

--- frontend/photorealistic-ai-frontend/src/app/app.component.ts
+++ frontend/photorealistic-ai-frontend/src/app/app.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { error } from 'console';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [],
 })
 export class AppComponent {
+  constructor(public httpservice: HttpClient,public cdr: ChangeDetectorRef) {}
   title = 'PhotoRealistic AI';
-  imageSrc: string | null = null;
+  imageSrc: any = null;
 
   // Handle button click to load a new image from the API
   loadImage() {
-    this.imageSrc = 'http://127.0.0.1:5000/generate_image'; // Replace with your API URL
+    this.httpservice.get("http://127.0.0.1:5000/generate_image").subscribe((res:any)=>{
+      this.imageSrc = 'data:image/png;base64,' + res['image'];
+      // this.imageSrc = atob(res['image']);÷
+      this.cdr.detectChanges();
+      this.cdr.markForCheck();
+    },
+    (error:any)=>console.log(error));
   }
 }
